e3bab5588bef4c27bd95f093c883ed34f339afc9
Gyandendra Prakash <prakash9.2020@gmail.com>
2022-02-04 16:45:55 +0530

    basic setup and mongoose schema created

---
+++ models/Todo.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const TodoSchema = new mongoose.Schema({
+    title:{
+        type: String,
+        require:[true, 'Please Add Title'],
+        maxlength: [40, 'Title cannot be more than 40 characters']
+    },
+    description:{
+        type: String,
+        require:[true, 'Please Add Description'],
+        maxlength: [400, 'Description cannot be more than 400 characters']
+    },  
+});
+
+module.exports = mongoose.models.Todo || mongoose.models('Todo', TodoSchema)
--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
 module.exports = {
-  reactStrictMode: true,
+  env:{
+    MONGO_URI: "mongodb+srv://Gyanendra:gyan123@cluster0.6o8qw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+  }
 }
